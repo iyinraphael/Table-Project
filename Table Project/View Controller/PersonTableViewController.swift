9bbcafbfaaa0a6f55ebe2649548799a6c03685ee
iyinraphael <iyinraphael@gmail.com>
2018-11-27 14:20:12 -0800

    added tableView code functionality

--- Table Project/View Controller/PersonTableViewController.swift
+++ Table Project/View Controller/PersonTableViewController.swift
@@ -30,20 +30,20 @@ class PersonTableViewController: UITableViewController {
         default:
             fatalError("Invalid section number")
         }
-        
-        return 0
+    
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
-        let cell = tableView.dequeueReusableCell(withIdentifier: AddPersonCell.reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: AddPersonCell.reuseIdentifier, for: indexPath) 
             return cell
         }
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PersonCell.reuseIdentifier, for: indexPath) as? PersonCell else {return UITableViewCell()}
-        cell.nameLabel.text = ModelController.shared.person(at: indexPath.row).name
-        cell.cohortLabel.text = ModelController.shared.person(at: indexPath.row).cohort
+        let person = ModelController.shared.person(at: indexPath.row)
+        cell.nameLabel.text = person.name
+        cell.cohortLabel.text = person.cohort
         return cell
 
     }
@@ -54,8 +54,12 @@ class PersonTableViewController: UITableViewController {
         let cohort = cell.cohortField.text ?? ""
         let person = Person(name: name, cohort: cohort)
         
+        cell.nameField.text = ""
+        cell.cohortField.text = ""
+        
         ModelController.shared.add(person:person)
         tableView.reloadData()
+        
         
     }
     
@@ -68,37 +72,14 @@ class PersonTableViewController: UITableViewController {
         }
     }
 
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        guard
+            let destination = segue.destination as? DetailViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {return}
+        let person = ModelController.shared.person(at: indexPath.row)
+        destination.person = person
     }
-    */
+
 
 }
